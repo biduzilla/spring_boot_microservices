886cafbd9e559738a80ba39d3cdbd60719c58489
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-07-01 19:01:57 -0300

    registro do users api no eureka

---
+++ PhotoAppApiUsers/src/main/kotlin/com/ricky/photoapp/api/users/controllers/UserController.kt
@@ -0,0 +1,15 @@
+package com.ricky.photoapp.api.users.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/users")
+class UserController {
+
+    @GetMapping("/status/check")
+    fun status():String{
+        return "Ok"
+    }
+}
