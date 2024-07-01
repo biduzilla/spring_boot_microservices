02e8e09145954253019106f696e53a3afb0f31e6
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-07-01 17:00:17 -0300

    eureka server e user api vazio

---
+++ PhotoAppApiUsers/src/main/kotlin/com/ricky/photoapp/api/users/PhotoAppApiUsersApplication.kt
@@ -0,0 +1,11 @@
+package com.ricky.photoapp.api.users
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhotoAppApiUsersApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhotoAppApiUsersApplication>(*args)
+}
---
+++ PhotoAppDiscoveryService/src/test/kotlin/com/ricky/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ricky.photoapp.discovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
