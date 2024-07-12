d0aa6d6f40e63938d7aa0dadad3cf65361e9a592
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-07-12 10:20:56 -0300

    users application update

--- ApiUser/src/main/kotlin/com/ricky/api/user/controllers/UserController.kt
+++ ApiUser/src/main/kotlin/com/ricky/api/user/controllers/UserController.kt
@@ -1,5 +1,6 @@
 package com.ricky.api.user.controllers
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/users")
-class UserController {
+class UserController(private val env:Environment) {
 
     @GetMapping("/status/check")
     fun status():String{
-        return "Ok"
+        return "Working port number " + env.getProperty("local.server.port")
     }
 }
